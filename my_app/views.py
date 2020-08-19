d01c453ecd7bbe9787fa8eef3721f2416bdfdf90
henrykwolek <you@example.comclear>
2020-08-19 15:25:03 -0700

    Ulepszony moduł SEARCH

--- my_app/views.py
+++ my_app/views.py
@@ -1,14 +1,29 @@
 from django.shortcuts import render
+from requests.compat import quote_plus
 import requests
 from bs4 import BeautifulSoup
+from . import models
 
 # Create your views here.
+
+BASE_CRAIGSLIST_URL = 'https://losangeles.craigslist.org/search/?query={}'
 
 def home(request):
     return render(request, 'base.html')
 
 def new_search(request):
     search = request.POST.get('search')
+    models.Search.objects.create(search=search)
+    final_url = BASE_CRAIGSLIST_URL.format(quote_plus(search))
+    response = requests.get(final_url)
+    data = response.text
+    soup = BeautifulSoup(data, features='html.parser')
+
+    post_listings = soup.find_all('li', {'class': 'result-row'})
+    post_title = post_listings[0].find(class_='result-title').text
+    post_url = post.find()
+
+    print(posts_titiles[0].get('href'))
     stuff_for_frontend = {
         'search': search,
     }
